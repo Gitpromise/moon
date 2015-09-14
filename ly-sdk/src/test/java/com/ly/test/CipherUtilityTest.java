ca51acb73ff7e66d0aaff61df332e1434df98e82
pengyong <pengyong@leya920.com>
2015-09-14 13:21:15 +0800

    junit

--- ly-sdk/src/test/java/com/ly/test/CipherUtilityTest.java
+++ ly-sdk/src/test/java/com/ly/test/CipherUtilityTest.java
@@ -1,5 +1,6 @@
 package com.ly.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.ly.sdk.utils.CipherUtility;
@@ -11,8 +12,9 @@ public class CipherUtilityTest {
         //CipherUtility test = new CipherUtility();
         //加密时 请确认env下有key为private的属性
        // System.out.println(CipherUtility.AES.encrypt("jdbc:mysql://127.0.0.1:3306/ly_sms?characterEncoding=UTF-8"));
-       System.out.println(CipherUtility.AES.encrypt("leyaftpadmin")+"],pwd:==["+CipherUtility.AES.encrypt("leyatestftp"));
-       System.out.println(CipherUtility.AES.decrypt("QWNduSrIhHTWwsG1aPX-bFQ9e85_fjEA7ItN0D1df2xB8GXooKKm3UvBrihOQvLO3Q9MyarnIIWQsYu3xDbANg"));
-       System.out.println(CipherUtility.AES.decrypt("QWNduSrIhHTWwsG1aPX-bFQ9e85_fjEA7ItN0D1df2xB8GXooKKm3UvBrihOQvLO3Q9MyarnIIWQsYu3xDbANg"));
+
+        Assert.assertEquals(CipherUtility.AES.encrypt("leyaftpadmin"),"_X2iVCvx-jMc0TlK74qODw");
+        Assert.assertEquals(CipherUtility.AES.encrypt("leyatestftp"),"vpb97KM2D5X_wO_BxlBMcw");
+        //Assert.assertEquals(CipherUtility.AES.encrypt("leyatestftp"),"error");
     }
 }
