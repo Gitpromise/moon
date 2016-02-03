6b6cd47952bc1cbd9da521eb539fec61eb9087bd
maguoqiang1991 <maguoqiang@leya920.com>
2016-02-03 18:17:53 +0800

    'dd'

--- moonservice/src/test/java/test/TestDemo.java
+++ moonservice/src/test/java/test/TestDemo.java
@@ -1,7 +1,22 @@
 package test;
 
+import com.moon.task.CommonTask;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * Created by mgq on 2016/1/29.
  */
+@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 public class TestDemo {
+
+    @Test
+    public void testTask(){
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+    }
 }
